79fa5152ba322c1aff368ea680957a25f484a6a4
Aqissiaq <aqissiaq@gmail.com>
2022-12-08 00:27:12 +0100

    :poop:

--- solutions/src/main.rs
+++ solutions/src/main.rs
@@ -6,10 +6,11 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main() {
-    let input = utils::get_input(6);
+    let input = utils::get_input(7);
     // let input = utils::read_file(&"src/test.txt".to_string());
-    println!("day 6 | part 1\n{}", day6::solve1(&input));
-    println!("day 6 | part 2\n{}", day6::solve2(&input));
+    println!("day 7 | part 1\n{}", day7::solve1(&input));
+    println!("day 7 | part 2\n{}", day7::solve2(&input));
 }
